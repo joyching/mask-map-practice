f5ca215f6ed11cae11028580fa8761ad759d661c
joylin <kanaheijoy@gmail.com>
2022-01-30 13:42:30 +0800

    行政區域套用至左選單

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <asideMenu />
+
+    <div class="mask-map" id="mask-map"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapActions } from 'vuex';
+import asideMenu from './components/asideMenu.vue';
 
 export default {
+  components: { asideMenu },
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    ...mapActions(['fetchLocations', 'fetchPharmacies'])
+  },
+  mounted() {
+    this.fetchLocations();
+    this.fetchPharmacies();
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" src="./style.scss"></style>
